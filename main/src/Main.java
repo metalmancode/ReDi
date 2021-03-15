4819e52b23302e660756af78a6d0aa9fc9740899
Metalman <rrashidi@gmail.com>
2021-03-15 21:13:31 +0100

    first Code upload

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,6 +1,15 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("First git commit");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide the first number: ");
+        int first = scanner.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(first +"x"+ i+ "="+(first*i) );
+
+        }
     }
 }
+
+
